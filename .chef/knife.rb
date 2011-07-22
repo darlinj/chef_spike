984f74173bf3ff89caf9d1045a764fe8c569ab97
darlinj <joe.darling@bt.com>
2011-07-22 10:59:18 +0100

    tidy up

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,11 +1,11 @@
 log_level                :info
 log_location             STDOUT
-node_name                'joe'
+node_name                'Joe'
 client_key               '/home/joe/code/multi-server-spike/.chef/joe.pem'
-#validation_client_name   'chef-validator'
 validation_key           '/home/joe/code/multi-server-spike/.chef/validation.pem'
 databag_encryption_key   '/home/joe/code/multi-server-spike/.chef/databag_encryption.key'
-chef_server_url          'http://109.144.10.241:4000'
+chef_server_url          'http://109.144.14.214:4000'
 cache_type               'BasicFile'
-cache_options( :path => '/home/joe/code/multi-server-spike/.chef/checksums' )
+cache_options( :path => '/home/joe/code/chefs-kitchen/.chef/checksums' )
+cookbook_path [ '/home/joe/code/chefs-kitchen/cookbooks' ]
 
